a003721fc3465fe32184f09527d38d214fac2f68
Mariano Tomás Pocztaruk <mpocztaruk_92@hotmail.com>
2022-11-22 20:04:21 -0300

    Add project files.

---
+++ BLL/EMPLEADO.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BLL
+{
+    public class EMPLEADO
+    {
+		private int id;
+
+		public int Id
+		{
+			get { return id; }
+			set { id = value; }
+		}
+
+		private string nombre;
+
+		public string Nombre
+		{
+			get { return nombre; }
+			set { nombre = value; }
+		}
+
+		private string apellido;
+
+		public string Apellido
+		{
+			get { return apellido; }
+			set { apellido = value; }
+		}
+
+		private float sueldoBruto;
+
+		public float SueldoBruto
+		{
+			get { return sueldoBruto; }
+			set { sueldoBruto = value; }
+		}
+
+	}
+}
---
+++ BLL/RECIBO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BLL
+{
+    public class RECIBO
+    {
+		private int id;
+
+		public int Id
+		{
+			get { return id; }
+			set { id = value; }
+		}
+		private EMPLEADO empleado;
+
+		public EMPLEADO Empleado
+		{
+			get { return empleado; }
+			set { empleado = value; }
+		}
+
+		private List<CONCEPTO> conceptos = new List<CONCEPTO>();
+
+		public List<CONCEPTO> Conceptos
+        {
+            get { return conceptos; }
+            set { conceptos = value; }
+        }
+        
+
+
+	}
+}
